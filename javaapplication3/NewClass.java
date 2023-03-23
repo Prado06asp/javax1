9da14ca5fd2bb4705f52936cf6b4d9cd2f8c64c8
Prado06asp <124307369+Prado06asp@users.noreply.github.com>
2023-03-23 19:07:58 -0300

    Add files via upload

---
+++ javaapplication3/NewClass.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class ComboBoxTest
+{
+    public static void main (string[]args)
+    {
+        ComboBoxFrame comboBoxFrame = new ComboxFrame();
+        comboBoxFrame.setDefautCloseOperation(Jframe.EXIT_ON_CLOSE);
+        comboBoxFrame.setSize(350,150);
+        comboBoxFrame.setVisible(true);
+    
+    }
+}
